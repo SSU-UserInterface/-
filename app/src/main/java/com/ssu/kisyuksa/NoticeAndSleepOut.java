c6cad2e3abbb0a1c479a03469f78a685ce879726
Cat-Hanbit <starp321@naver.com>
2023-11-13 01:24:21 +0900

    [refactor] 클래스 이름 변경

--- app/src/main/java/com/ssu/kisyuksa/NoticeAndSleepOut.java
+++ app/src/main/java/com/ssu/kisyuksa/NoticeAndSleepOut.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NoticeAndSleepover#newInstance} factory method to
+ * Use the {@link NoticeAndSleepOut#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NoticeAndSleepover extends Fragment {
+public class NoticeAndSleepOut extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class NoticeAndSleepover extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NoticeAndSleepover() {
+    public NoticeAndSleepOut() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class NoticeAndSleepover extends Fragment {
      * @return A new instance of fragment NoticeAndSleepover.
      */
     // TODO: Rename and change types and number of parameters
-    public static NoticeAndSleepover newInstance(String param1, String param2) {
-        NoticeAndSleepover fragment = new NoticeAndSleepover();
+    public static NoticeAndSleepOut newInstance(String param1, String param2) {
+        NoticeAndSleepOut fragment = new NoticeAndSleepOut();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
