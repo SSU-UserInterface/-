c4adac7311c5ab1ba332337a4a3f30331c9bd4f2
Yerin Song <yrsong0915@gmail.com>
2023-11-13 17:49:29 +0900

    [setting] 바텀네비 사용 위해 gradle 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,9 @@ android {
 
 dependencies {
 
+    //material design
+    implementation ("com.google.android.material:material:1.1.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
