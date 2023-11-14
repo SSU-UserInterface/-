405c7c0817f8b876c5fc99422b622181e8ff93b2
isaac <gladisaac5441@gmail.com>
2023-11-14 23:50:54 +0900

    add : 라운지 메인 화면 및 부속화면(공동배달, ott, 헬스장, 채탱) UI 제작

---
+++ app/src/main/java/com/ssu/kisyuksa/activity_rounge_chat.java
@@ -0,0 +1,14 @@
+package com.ssu.kisyuksa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_rounge_chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rounge_chat);
+    }
+}
