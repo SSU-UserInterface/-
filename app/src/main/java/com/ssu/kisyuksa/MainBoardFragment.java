1e043729ed44c87bd75e953ed2f552933cb638e0
Cat-Hanbit <starp321@naver.com>
2023-11-15 15:21:37 +0900

    [refactor] 이름 변경 NoticeBoard -> MainBoard

--- app/src/main/java/com/ssu/kisyuksa/MainBoardFragment.java
+++ app/src/main/java/com/ssu/kisyuksa/MainBoardFragment.java
@@ -59,6 +59,6 @@ public class MainBoardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notice_board, container, false);
+        return inflater.inflate(R.layout.fragment_main_board, container, false);
     }
 }
