29c62f5e418747945599d9c10b9d631cdb6f484e
Yerin Song <yrsong0915@gmail.com>
2023-11-21 23:20:27 +0900

    [feat] 바텀네비 기능 구현

--- app/src/main/java/com/ssu/kisyuksa/BnvActivity.java
+++ app/src/main/java/com/ssu/kisyuksa/BnvActivity.java
@@ -2,13 +2,44 @@ package com.ssu.kisyuksa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import com.ssu.kisyuksa.databinding.ActivityBnvBinding;
 
 public class BnvActivity extends AppCompatActivity {
+
+    private ActivityBnvBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bnv);
+        binding = ActivityBnvBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bnvMain.setOnNavigationItemSelectedListener(menuItem -> {
+            if (menuItem.getItemId() == R.id.bnv_check) {
+                replaceFragment(new NoticeAndSleepOutFragment());
+                return true;
+            } else if (menuItem.getItemId() == R.id.bnv_board) {
+                replaceFragment(new MainBoardFragment());
+                return true;
+            } else if (menuItem.getItemId() == R.id.bnv_person) {
+                openMyPageActivity();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(binding.flContent.getId(), fragment)
+                .commit();
+    }
+    private void openMyPageActivity() {
+        Intent intent = new Intent(this, MyPageActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
