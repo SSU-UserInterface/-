5df49deea2725dba78237889d752ad8921784fa8
Cat-Hanbit <starp321@naver.com>
2023-11-23 23:14:53 +0900

    [feat] firebase 서버 추가 1. gradle - app  1) id : google-servies 추가  2) dependencies 추가
    2. gradle - project
     id : google-services 추가
    
    3. json file 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // Add the Google services Gradle plugin 파이어베이스 추가
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -49,4 +52,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase 추가
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
